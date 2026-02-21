4213c65ea629e5aada1b273db0eb58e334be4c01
kutO-O <fixplayigor@gmail.com>
2026-02-22 01:27:32 +0300

    fix: Add PatternGuidedPlanner to exports

--- core/learning/__init__.py
+++ core/learning/__init__.py
@@ -2,8 +2,10 @@
 
 from core.learning.success_pattern import SuccessPattern
 from core.learning.learning_engine import LearningEngine
+from core.learning.pattern_guided_planner import PatternGuidedPlanner
 
 __all__ = [
     "SuccessPattern",
     "LearningEngine",
+    "PatternGuidedPlanner",
 ]
