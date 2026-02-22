9328b63f0bf3c404407142aec94173acdd31d2cf
kutO-O <fixplayigor@gmail.com>
2026-02-22 14:19:47 +0300

    feat(integration): add __init__.py for new modules
    - multi_agent package
    - memory subpackage additions
    - self_evolution package

---
+++ core/multi_agent/__init__.py
@@ -0,0 +1,7 @@
+"""Multi-agent coordination system"""
+
+from core.multi_agent.task_delegation import TaskDelegation
+from core.multi_agent.consensus_builder import ConsensusBuilder
+from core.multi_agent.agent_roles import AgentRoles
+
+__all__ = ["TaskDelegation", "ConsensusBuilder", "AgentRoles"]
