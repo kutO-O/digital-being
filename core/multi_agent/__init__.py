ab163c9e077c62b2954ce2526be5d35a7d0becc3
kutO-O <fixplayigor@gmail.com>
2026-02-22 17:54:28 +0300

    Add: Export autoscaler from multi_agent module

--- core/multi_agent/__init__.py
+++ core/multi_agent/__init__.py
@@ -1,7 +1,15 @@
-"""Multi-agent coordination system"""
+"""Multi-Agent collaboration components."""
 
 from core.multi_agent.task_delegation import TaskDelegation
 from core.multi_agent.consensus_builder import ConsensusBuilder
-from core.multi_agent.agent_roles import AgentRoleManager
+from core.multi_agent.agent_roles import AgentRoleManager, AgentRole
+from core.multi_agent.autoscaler import AgentAutoscaler, ScalingPolicy
 
-__all__ = ["TaskDelegation", "ConsensusBuilder", "AgentRoleManager"]
+__all__ = [
+    "TaskDelegation",
+    "ConsensusBuilder",
+    "AgentRoleManager",
+    "AgentRole",
+    "AgentAutoscaler",
+    "ScalingPolicy",
+]
