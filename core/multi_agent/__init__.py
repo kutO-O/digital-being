fbcf9ac0f53e56bb0b23fb7f6918262512331684
kutO-O <fixplayigor@gmail.com>
2026-02-22 14:31:45 +0300

    fix(multi_agent): correct AgentRoles import to AgentRoleManager
    Fixed import error where AgentRoles was imported but the actual
    class name is AgentRoleManager in agent_roles.py

--- core/multi_agent/__init__.py
+++ core/multi_agent/__init__.py
@@ -2,6 +2,6 @@
 
 from core.multi_agent.task_delegation import TaskDelegation
 from core.multi_agent.consensus_builder import ConsensusBuilder
-from core.multi_agent.agent_roles import AgentRoles
+from core.multi_agent.agent_roles import AgentRoleManager
 
-__all__ = ["TaskDelegation", "ConsensusBuilder", "AgentRoles"]
+__all__ = ["TaskDelegation", "ConsensusBuilder", "AgentRoleManager"]
